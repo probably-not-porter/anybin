3c7d56354c5b6e00c8531062bcb248e7637b955c
probably-not-porter <pelibby16@earlham.edu>
2023-03-15 01:09:58 -0400

    work on routing

--- index.js
+++ index.js
@@ -7,10 +7,15 @@
 require('dotenv').config();
 const { QuickDB } = require("quick.db");
 const express = require('express');
+var bodyParser = require('body-parser')
 var path = require('path');
 var favicon = require('serve-favicon');
-const { resolveSoa } = require('dns');
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // =========== Front end setup =========== //
 const e_app = express();
@@ -20,35 +25,41 @@ e_app.get('/', function(_req, res) {
     })
 });
 
+
 // =========== Quick.DB setup =========== //
 const db = new QuickDB();
 
 // GETS
-e_app.get('/user', async function(req, res) {
+e_app.get('/api/user', async function(req, res) {
   console.log(req.query.userid);
-  let obj = await db.get( req.query.userid );
+  let obj = await db.get( "user/" + req.query.userid );
   res.send(obj);
 });
-e_app.get('/bin', async function(req, res) {
-  let obj = await db.get( req.query.userid + "/" + req.query.binid );
+e_app.get('/api/bin', async function(req, res) {
+  let obj = await db.get( "bin/" + req.query.binid );
   res.send(obj);
 });
-e_app.get('/item', async function(req, res) {
-  let obj = await db.get( req.query.userid + "/" + req.query.binid + "/" + req.query.itemid );
+e_app.get('/api/item', async function(req, res) {
+  let obj = await db.get( "item/" + req.query.itemid );
   res.send(obj);
 });
 
 // PUTS
-e_app.post('/user', async function(req, res) {
+e_app.post('/api/user', jsonParser, async function(req, res) {
   console.log(req.body);
-  //await db.set(req.query.userid, req.query.value);
+  await db.set("user/" + req.body.userid, req.body.value);
   res.send("success!");
 });
-e_app.post('/bin', async function(req, res) {
 
+e_app.post('/api/bin', jsonParser, async function(req, res) {
+  console.log(req.body);
+  await db.set("bin/" + req.body.binid, req.body.value);
+  res.send("success!");
 });
-e_app.post('/item', async function(req, res) {
-
+e_app.post('/api/item', jsonParser, async function(req, res) {
+  console.log(req.body);
+  await db.set("item/" + req.body.itemid, req.body.value);
+  res.send("success!");
 });
 
 async function create_user(){
